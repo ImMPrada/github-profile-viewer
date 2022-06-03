37878d9a79c5ee2765565c920657d02fa79718c7
miguel-prada <miguel@modernadvisor.ca>
2022-06-03 18:18:25 -0500

    Include GlobalContext

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-
-const mode = 'dark'
+import React, { useContext } from "react";
+import { GlobalContext } from "../../contexts/GlobalContext";
 
 
 const Header = () => {
+  const { theme, setTheme } = useContext(GlobalContext);
 
   return (
-    <div className = {`header header-${mode}`}>
+    <div className = {`header header-${theme}`}>
       <h1>devfinder</h1>
       <button>DARK</button>
     </div>
