69625732585e7ddf27668858319931580753399d
miguel-prada <miguel@modernadvisor.ca>
2022-06-03 16:52:15 -0500

    move into sass

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
+import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>HOLI</h1>
-      </header>
+      <h1>HOLI</h1>
     </div>
   );
 }
