c208d1097351e6d8f28a48df98336e0437f4996f
miguel-prada <miguel@modernadvisor.ca>
2022-06-03 18:13:21 -0500

    Implement context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import { GlobalProvider } from './contexts/GlobalContext';
 
 function App() {
   return (
     <div className="App">
-      <h1>HOLI</h1>
+      <GlobalProvider>
+        <h1>HOLI</h1>
+      </GlobalProvider>
     </div>
   );
 }
