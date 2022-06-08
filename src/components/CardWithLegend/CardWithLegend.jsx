297e47d5376d0dc33e9be6f28992a0172f98d85f
immprada <im.mprada@gmmail.com>
2022-06-08 09:21:32 -0500

    Add styles mobile first

--- src/components/CardWithLegend/CardWithLegend.jsx
+++ src/components/CardWithLegend/CardWithLegend.jsx
@@ -4,18 +4,21 @@ import { format } from 'date-fns';
 
 
 const CardWithLegend = () => {
-  const { git } = useContext(GlobalContext)
+  const { git, theme } = useContext(GlobalContext)
   const dateFromDate = format(new Date(git.created_at), 'yyyy/MM/dd')
 
   return (
-    <div className="card-label">
-      <img src={git.avatar_url} alt="" />
-      <div className="card-label__legend">
-        <div className="card-label__legend__head">
-          <h3>{git.name}<br/><a href={git.html_url}>@{git.login}</a></h3>
-          <p>joined at: {dateFromDate}</p>
+    <div className={`card-label card-label-${theme}`}>
+      <div className="card-label__head">  
+        <img src={git.avatar_url} alt="" />
+        <div className="card-label__head__legend">
+          <div className="card-label__head__legend__info">
+            <h3>{git.name}<br/><a href={git.html_url}>@{git.login}</a></h3>
+            <p>joined at: {dateFromDate}</p>
+          </div>
         </div>
       </div>
+      <p className="card-label__legend" >Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros.</p>
     </div>
   )
 }
