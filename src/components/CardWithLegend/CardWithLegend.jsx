7dc56912a512c8fe948ce2c5cfc349bffdef1638
immprada <im.mprada@gmmail.com>
2022-06-07 21:50:17 -0500

    Add component

---
+++ src/components/CardWithLegend/CardWithLegend.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const CardWithLegend = () => {
+
+  return (
+    <h1>HOLI</h1>
+  )
+}
+
+export default CardWithLegend;
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Header } from './Header'
 export { default as MainContainer } from './MainContainer'
+export { default as CardWithLegend } from './CardWithLegend'
