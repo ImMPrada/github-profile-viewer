7e1dad386a20b82ce986f34c68a2da425384af55
immprada <im.mprada@gmmail.com>
2022-06-07 22:10:43 -0500

    Add card mockup

--- src/components/CardWithLegend/CardWithLegend.jsx
+++ src/components/CardWithLegend/CardWithLegend.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../../contexts/GlobalContext";
 
 
 const CardWithLegend = () => {
+  const { git } = useContext(GlobalContext)
 
   return (
-    <h1>HOLI</h1>
+    <div className="card-label">
+      <img src={git.avatar_url} alt="" />
+      <div className="card-label__legend">
+        <div className="card-label__legend__head">
+          <h3>{git.name}</h3>
+          <p>{git.created_at}</p>
+        </div>
+        <a href={git.html_url}>{git.login}</a>
+      </div>
+    </div>
   )
 }
 
