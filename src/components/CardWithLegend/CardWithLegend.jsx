77728b50f08d4eb3804f140f69c690c546cff262
immprada <im.mprada@gmmail.com>
2022-06-07 22:55:45 -0500

    Add date-fns

--- src/components/CardWithLegend/CardWithLegend.jsx
+++ src/components/CardWithLegend/CardWithLegend.jsx
@@ -1,20 +1,20 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "../../contexts/GlobalContext";
+import { format } from 'date-fns';
 
 
 const CardWithLegend = () => {
   const { git } = useContext(GlobalContext)
+  const dateFromDate = format(new Date(git.created_at), 'yyyy/MM/dd')
 
   return (
     <div className="card-label">
       <img src={git.avatar_url} alt="" />
       <div className="card-label__legend">
         <div className="card-label__legend__head">
-          <h3>{git.name}</h3>
-          <p>{git.created_at}</p>
+          <h3>{git.name}<br/><a href={git.html_url}>@{git.login}</a></h3>
+          <p>joined at: {dateFromDate}</p>
         </div>
-        <a href={git.html_url}>{git.login}</a>
-        <p>{git.bio || 'There is no bio for this user...'}</p>
       </div>
     </div>
   )
