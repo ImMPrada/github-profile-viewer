9c5e25d51879053b43259eda6be6067e3cc2bf0b
immprada <im.mprada@gmmail.com>
2022-06-08 22:30:05 -0500

    Add styles: mobile

--- src/components/DataBox/DataBox.jsx
+++ src/components/DataBox/DataBox.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../../contexts/GlobalContext";
 
 
 const DataBox = () => {
+  const { theme } = useContext(GlobalContext);
 
   return (
-    <h1>HOLI</h1>
+    <div className={`data-box data-box-${theme}`}>
+      <div className="data-box__info">
+        <p>Repos</p>
+        <p>9</p>
+      </div>
+      <div className="data-box__info">
+        <p>Followers</p>
+        <p>3938</p>
+      </div>
+      <div className="data-box__info">
+        <p>Following</p>
+        <p>9</p>
+      </div>
+    </div>
   )
 }
 
