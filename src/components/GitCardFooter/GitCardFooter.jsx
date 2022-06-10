9695e3357129dd4fca81e6de31f50a2307b7bef1
immprada <im.mprada@gmmail.com>
2022-06-10 15:06:46 -0500

    Add link tag to Blog field at footer

--- src/components/GitCardFooter/GitCardFooter.jsx
+++ src/components/GitCardFooter/GitCardFooter.jsx
@@ -5,6 +5,20 @@ import { LocationPin, Url, Twitter, Offices } from "../../icons";
 
 const GitCardFooter = () => {
   const { theme, git } = useContext(GlobalContext);
+  const getLinkData = (link) => {
+    if (!link) return { linkTo: null, label: null }
+    if (link.includes('http')) {
+      return {
+        linkTo: link,
+        label: link.slice(-1) === '/' ? link.split('/').slice(-2,-1)[0] : link.split('/').slice(-1),
+      }
+    }
+
+    return {
+      linkTo: '',
+      label: link,
+    }
+  }
 
   return (
     <div className = {`git-card-footer git-card-footer-${theme}`}>
@@ -14,7 +28,9 @@ const GitCardFooter = () => {
       </div>
       <div className={`git-card-footer__element${git.blog ? '' : '-null'}`}>
         <Url /> 
-        <p>{git.blog || 'Not Available'}</p>
+        <a href = {getLinkData(git.blog).linkTo} >
+          {getLinkData(git.blog).label || 'Not Available'}
+        </a>
       </div>
       <div className={`git-card-footer__element${git.twitter_username ? '' : '-null'}`}>
         <Twitter /> 
