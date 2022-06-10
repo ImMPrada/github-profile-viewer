339c9c1af033b8d59cd7e0f00aa8369c15fa0656
miguel <26731448+ImMPrada@users.noreply.github.com>
2022-06-10 12:37:20 -0500

    008 components git card footer (#9)
    * GitCadFooter
    
    * GitCardFooter: initial mockup
    
    * GitCardFooter: styles mockup
    
    * Add icons
    
    * Add styles: mobile first
    
    * Add styles: not mobile
    
    Co-authored-by: immprada <im.mprada@gmmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MainContainer, Header, SearchBox, CardWithLegend } from './components'
+import { MainContainer, Header, SearchBox, CardWithLegend, GitCardFooter } from './components'
 import { GlobalProvider } from './contexts/GlobalContext';
 
 const App = () => {
@@ -10,6 +10,7 @@ const App = () => {
           <Header />
           <SearchBox />
           <CardWithLegend />
+          <GitCardFooter />
         </MainContainer>
       </GlobalProvider>
     </div>
