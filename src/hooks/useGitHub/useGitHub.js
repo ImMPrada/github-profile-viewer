1dcff9cf29e104eb6e08e41221f638eafe82046e
immprada <im.mprada@gmail.com>
2022-06-20 18:01:11 -0500

    Create useGitHub hook

--- src/hooks/useGitHub/useGitHub.js
+++ src/hooks/useGitHub/useGitHub.js
@@ -3,13 +3,35 @@ import {
   useState,
   useEffect
 } from "react";
+import { GlobalContext } from '../../contexts/GlobalContext'
+import axios from 'axios';
+
+const baseURL = 'https://api.github.com/users/'
 
 
 const useGitHub = (gists, setGists) => {
+  const {
+    setLoading,
+    setGithubProfile,
+  } = useContext(GlobalContext)
 
+  const getProfile = (profileName) => {
+    setLoading(true)
+
+    axios.get(`${baseURL}${profileName}`)
+      .then(res => {
+
+        setGithubProfile(res.data)
+      })
+      .catch(err => {
+
+        setGithubProfile('NotFoundError')
+        setLoading(false)
+      })
+  }
 
   return {
-
+    getProfile,
   }
 }
 
