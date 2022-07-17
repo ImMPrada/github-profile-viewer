177c2cd284bf518e8ff2ec570b3573a777679890
immprada <im.mprada@gmail.com>
2022-07-17 14:51:07 -0500

    Show butoon only when there is repos

--- src/components/ReposList/ReposList.jsx
+++ src/components/ReposList/ReposList.jsx
@@ -10,16 +10,18 @@ import './styles.scss';
 
 const ReposList = () => {
   const [hidden, setHidden] = useState(true);
-  const { theme } = useContext(GlobalContext);
+  const { state, theme } = useContext(GlobalContext);
 
   return (
     <div className = {`repos-list repos-list-${theme}`}>
       <div className={`repos-list__content repos-list--hiden__${hidden}`}>
         Here will be displayed the list of repos
       </div>
-      <button className="repos-list__button" onClick={() => setHidden(!hidden)}>
-        {hidden ? 'Show list of repos' : 'Hide list of repos'}
-      </button>
+      { state.repos.length > 0 && 
+        <button className="repos-list__button" onClick={() => setHidden(!hidden)}>
+          {hidden ? 'Show list of repos' : 'Hide list of repos'}
+        </button>
+      } 
     </div>
   )
 }
