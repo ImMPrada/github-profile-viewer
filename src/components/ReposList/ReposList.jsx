a9a80b5c109349c0134b71bada21d92e8d29d24b
immprada <im.mprada@gmail.com>
2022-07-25 20:58:37 -0500

    Add repos to ReposList

--- src/components/ReposList/ReposList.jsx
+++ src/components/ReposList/ReposList.jsx
@@ -1,23 +1,57 @@
 import React, { useContext, useState } from "react";
+import { useEffect } from "react";
 import { GlobalContext } from "../../contexts/GlobalContext";
+import { useGitHub } from "../../hooks";
 import './styles.scss';
 
+const Repo = ({
+  repo, 
+  getLanguages
+}) => {
+  const {
+    name,
+    description,
+    liveDemo,
+    url,
+    updatedAt,
+    languagesUrl,
+  } = repo
+
+  useEffect(() => {
+    if(!languagesUrl) return
+
+    getLanguages(languagesUrl)
+  }, [languagesUrl])
+
+  return (
+    <div className="repos-list__content__item">
+      <a href={url}><h4>{name}</h4></a>
+      <a href={liveDemo}><h4>{description || liveDemo ? 'Click for a live demo' : null}</h4></a>
+      <h3>{updatedAt}</h3>
+    </div>
+  )
+}
 
 const ReposList = () => {
   const [hidden, setHidden] = useState(true);
   const { state, theme } = useContext(GlobalContext);
+  const { getLanguages } = useGitHub();
 
-  // const repos = (
-  //   state.repos.map(repo => {
-  //     return <h3>{repo.name}</h3>
-  //   })
-  // )
+  const repos = (
+    state.repos.map(repo => (
+      <Repo
+        key={repo.name}
+        repo={repo}
+        getLanguages={getLanguages}
+      />
+    ))
+  )
 
   return (
     <div className = {`repos-list repos-list-${theme}`}>
       <div className={`repos-list__content repos-list--hiden__${hidden}`}>
         Here will be displayed the list of repos
-        {/* {repos} */}
+        {repos}
       </div>
       { state.repos.length > 0 && 
         <button className="repos-list__button" onClick={() => setHidden(!hidden)}>
