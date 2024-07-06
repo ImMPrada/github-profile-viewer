57bb726b494e0957e77dd31d66ea2ce83dd2d5bc
miguel <26731448+ImMPrada@users.noreply.github.com>
2024-07-06 17:10:30 -0500

    Update ErrorMessage.jsx

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,11 +1,8 @@
 import React, { useContext } from "react";
-import { GlobalContext } from "../../contexts/GlobalContext";
 import './styles.scss';
 
 
 const ErrorMessage = () => {
-  const { theme } = useContext(GlobalContext);
-
   return (
     <div className = {`error-message`}>
       <h4>ERROR!</h4>
